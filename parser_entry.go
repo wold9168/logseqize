6d241556d9561c09078049d6d607d0e7a078d874
wold9168 <moewold@outlook.com>
2026-03-24 21:53:35 +0800

    Add docstrings to public functions
    Signed-off-by: wold9168 <moewold@outlook.com>

--- parser_entry.go
+++ parser_entry.go
@@ -5,12 +5,14 @@ import (
 	"github.com/gomarkdown/markdown/parser"
 )
 
+// ConvertByString converts Standard Markdown String into Logseq-style Markdown String
 func ConvertByString(input string) (output string, err error) {
 	inputBytes := []byte(input)
 	output,err = Convert(inputBytes)
 	return
 }
 
+// Convert converts Standard Markdown []byte into Logseq-style Markdown String
 func Convert(input []byte) (output string, err error) {
 	extensions := parser.CommonExtensions
 	p := parser.NewWithExtensions(extensions)
@@ -19,6 +21,8 @@ func Convert(input []byte) (output string, err error) {
 	output, err = ast.ToString(doctree), nil
 	return
 }
+
+// DeConvert haven't been implemented yet.
 func DeConvert(input []byte) (output []byte, err error) {
 	return
 }
