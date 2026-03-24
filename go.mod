b8d17bcb2b77ed92f56fc4c22c46600dac430e40
wold9168 <moewold@outlook.com>
2026-03-24 20:55:45 +0800

    Initialize Go module and add basic parser entry points
    Signed-off-by: wold9168 <moewold@outlook.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wold9168/logseqize
+
+go 1.25.5
+
+require github.com/gomarkdown/markdown v0.0.0-20260217112301-37c66b85d6ab // indirect
---
+++ parser_entry.go
@@ -0,0 +1,8 @@
+package logseqize
+
+func Convert(input string) (output string, err error) {
+	return
+}
+func DeConvert(input string) (output string, err error) {
+	return
+}
