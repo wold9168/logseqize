1d701fd6eca5e71f206fed924cbfb25d67bce7aa
wold9168 <moewold@outlook.com>
2026-03-25 10:43:22 +0800

    Remove AST-based transformation logic
    Signed-off-by: wold9168 <moewold@outlook.com>

--- example/main.go
+++ example/main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/wold9168/logseqize"
-)
+import "fmt"
 
 func main() {
-	md, err := os.ReadFile("markdown_foo.md")
-	if err != nil {
-		fmt.Println("ReadFile Failed:", err)
-		return
-	}
-	fmt.Print(logseqize.Convert(md))
+	fmt.Println("WIP")
 }
